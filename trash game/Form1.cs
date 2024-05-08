104c1b28898a232314535a2cf6176f4f7daaf4c3
LENOVO T470 <LENOVO T470@DESKTOP-O5M461H>
2024-05-08 23:37:54 +0800

    Add project files.

---
+++ trash game/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trash_game
+{
+    public partial class trashgame : Form
+    {
+        public trashgame()
+        {
+            InitializeComponent();
+        }
+
+        private void gameload(object sender, EventArgs e)
+        {
+            
+            int w = Screen.PrimaryScreen.Bounds.Width;
+            int h = Screen.PrimaryScreen.Bounds.Height;
+            this.Location = new Point(0, 0);
+            this.Size = new Size(w, h);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
